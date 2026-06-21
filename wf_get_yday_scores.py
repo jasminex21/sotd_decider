faf274fb88bebaa5bf18800f1e0bb543f49fabe5
jasminex21 <jasminexu7003@icloud.com>
2026-06-20 20:04:30 -0500

    timezone issues should be fixed

--- wf_get_yday_scores.py
+++ wf_get_yday_scores.py
@@ -1,5 +1,5 @@
-import SOTDecider
-from SOTDecider import SOTDecider
+import wf_SOTDecider
+from wf_SOTDecider import SOTDecider
 
 import os
 import pytz
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     load_dotenv()
 
     # get yesterday's date midnight timestamp
-    # cst_timezone = pytz.timezone("America/Chicago")
-    yday_date = datetime.now()- timedelta(days=1)# .astimezone(cst_timezone) - timedelta(days=1)
+    cst_timezone = pytz.timezone("America/Chicago")
+    yday_date = datetime.now().astimezone(cst_timezone) - timedelta(days=1)
     yday_date = yday_date.replace(hour=23, minute=59, second=59, microsecond=59)
 
     decider = SOTDecider(lastfm_api_key=os.environ.get("LASTFM_API_KEY"),
-                         range_option="last 3 days",
+                         range_option="last 4 days",
                          end_time=yday_date)
     decider.get_scores()
